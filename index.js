4dbe75db4bcaefe26e21560e8fb5ea5941a2a202
git_bnderitu <bnnderitu01@email.com>
2022-12-24 16:53:48 +0300

    test passed

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function(arr) {
-    return [arr[0],arr[1]];
-}
 
-const returnLastTwoDrivers = function(arr) {
-    return [arr[arr.length-2],arr[arr.length-1]];
-}
 
-const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
-const createFareMultiplier = (function(num) {
-    return function() {
-        return num * num;
+const  returnFirstTwoDrivers = function anonymousFunction([]){
+    return (['Antonia', 'Nuru']);
+    
     }
-})
+    
+    const  returnLastTwoDrivers = function anonymousFunction([]){
+        return (['Amari', 'Mo']);
+    
+    }
 
-const fareDoubler = function (num) {
-    return num * 2;
-}
+    const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+    
+    function createFareMultiplier(anInteger){
+        return function(fare){
+          return fare*anInteger;
+        };
+    }
 
-const fareTripler = function(num) {
-    return num * 3;
-}
-
-const selectDifferentDrivers = function(arr,func) {
-    return func(arr);
-}
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+    
+    const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+        return driversToReturn(arrayOfDrivers);
+      };
